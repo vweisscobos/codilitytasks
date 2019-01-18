d63dbc202b4aba7e054e64768329115a7fbbb81e
vweisscobos <v.weisscobos@gmail.com>
2019-01-18 10:58:12 -0200

    Create MaxProductOfThree.js

---
+++ Lesson6/MaxProductOfThree.js
@@ -0,0 +1,44 @@
+
+
+function compare(num1, num2) {
+    return num1 - num2;
+}
+
+function solution(A) {
+    var nums = [],
+        prod = 1,
+        i = 1
+    
+    let pos = [];
+    let neg = [];
+    
+    A.sort(compare);
+    
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] < 0) neg.push(A[i]);
+        if (A[i] > 0) pos.push(A[i]);
+    }
+
+    if (pos.length <= 1 && neg.length <= 1) return 0;
+    
+    if ((pos.length === 1 || pos.length === 2) && neg.length > 1) {
+        return neg[0] * neg[1] * pos[pos.length -1];
+    }
+    
+    if (pos.length > 2 && neg.length > 1) {
+        let a = neg[0] * neg[1] * pos[pos.length -1],
+            b = pos[pos.length - 1] * pos[pos.length - 2] * pos[pos.length - 3]
+
+        if (a > b) return a;
+        return b;
+    }
+    
+    for (let i = A.length - 1; i > A.length - 4; i--) {
+        let n = A[i];
+        
+        prod *= n;
+    }
+
+    return prod;
+}
+
