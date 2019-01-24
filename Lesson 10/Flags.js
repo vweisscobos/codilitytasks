d3c6ac38d63a84c9b5ef1371f5736901b733d11f
vweisscobos <v.weisscobos@gmail.com>
2019-01-24 12:33:00 -0200

    Create Flags.js

---
+++ Lesson 10/Flags.js
@@ -0,0 +1,27 @@
+function solution(A) {
+    var peaks = [],
+        distances = [0];
+        
+    for (let i = 1; i < A.length; i++) {
+        if (A[i] > A[i - 1] && A[i] > A[i + 1]) peaks.push(i);
+    }
+    
+    if (peaks.length === 0) return 0;
+    if (peaks.length === 1) return 1;
+    
+    for (let i = peaks.length; i > 0; i--) {
+        let counter = 1,
+            lastSet = peaks[0];
+        
+        if ((peaks[peaks.length - 1] - peaks[0]) / (i - 1) < i) continue;
+       
+        for (let j = 1; j < peaks.length; j++) {
+            if (peaks[j] - lastSet >= i) {
+                counter++;
+                lastSet = peaks[j];
+            }
+        }
+                
+        if (counter >= i) return i;
+    }
+}
